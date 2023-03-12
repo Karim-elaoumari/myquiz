a56d3e44041af8e7f7f26dc553cef865b23ebeb6
ELKABLI <amina.elkabli1@gmail.com>
2023-03-12 17:43:14 +0100

    try the same errorr21

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -4,15 +4,13 @@ import Card from './card.vue'
 const quizes1 = inject('quizes')
 const quizmain = ref(quizes1)
 const quizes = ref(quizes1)
-const search = ref("")
-  if(search.value==''){
+const search = ref('')
+  if(search.value===''){
     quizes.value = quizes1.value
   }
   else{
     quizes.value = quizmain.value.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
   }
-  
-
 </script>
 <template>
         <div class=" text-center">
@@ -25,7 +23,7 @@ const search = ref("")
             <input class=" form-control" v-model="search" type="search" placeholder="Search Quiz ..." >
          </div>
          <div class="col">
-            <button @click="$emit('add-quiz')" type="button" class=" btn btn-success">Quiz</button>
+            <button @click="$emit('add-quiz')" type="button" class=" btn btn-success"> Add Quiz</button>
          </div>
 
         </div>
