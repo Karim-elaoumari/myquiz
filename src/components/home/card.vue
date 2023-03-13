32d9166b2198bc45cfcff07621df515415887c31
karim elaoumari <elaoumarikarim@gmail.com>
2023-03-13 15:21:49 +0100

    done the chnages on the cards

--- src/components/home/card.vue
+++ src/components/home/card.vue
@@ -6,7 +6,7 @@ const {quiz} = defineProps(['quiz'])
 </script>
 <template>
     <div  class="col-3 card mt-5 p-0" style="width: 14rem;cursor: pointer;">
-          <img :src="quiz.img" class="card-img-top " alt="..." style="height:9rem;">
+          <img :src="quiz.img" class="card-img-top " alt="..." style="height:7rem;">
           <div class="card-body">
             <p class="card-text fs-4">{{quiz.name}}</p>
             <p class="card-text fs-6">{{quiz.questions.length}} questions</p>
